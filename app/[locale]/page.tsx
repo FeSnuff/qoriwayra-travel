7e1418e5cb1ca675b6013f842ab7e3a5d1df63f2
FeSnuff <46802768+FeSnuff@users.noreply.github.com>
2026-09-07 22:29:47 -0400

    Delete app directory

--- app/[locale]/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/[locale]/page.tsx
-export async function generateStaticParams() {
-  return [{ locale: 'es' }, { locale: 'en' }];
-}
-
-export default function Home() {
-  return <h1>¡Bienvenido!</h1>;
-}
